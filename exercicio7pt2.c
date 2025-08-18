5fc740e6a3a7f20b93a0f0cef40cb21c093cbb90
Victor Gonçalves <94809017+victorgcv@users.noreply.github.com>
2025-08-18 17:50:42 -0300

    Create exercicio7pt2.c
    exercicio 7 parte 2
    
    7. Leia dois valores para as variáveis A e B, e efetue a troca dos valores de forma que a
    variável A passe a ter o valor da variável B e que a variável B passe a ter o valor da
    variável A. Ao final, exiba os resultados. Obs.: deverão ser feitos dois algoritmos: no
    primeiro poderá ser usada uma terceira variável. No segundo somente as variáveis A e
    B.

---
+++ exercicio7pt2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    /* 7. Leia dois valores para as variáveis A e B, e efetue a troca dos valores de forma que a
+variável A passe a ter o valor da variável B e que a variável B passe a ter o valor da
+variável A. Ao final, exiba os resultados. Obs.: deverão ser feitos dois algoritmos: no
+primeiro poderá ser usada uma terceira variável. No segundo somente as variáveis A e
+B. */
+    
+    // criar a variavel a
+    // criar a variavel b
+    
+    // pedir os valores
+    // imprimir os valores trocados
+    
+    int a;
+    int b;
+    
+    printf("Digite o valor da variável A:");
+    scanf("%d", &a);
+    
+    printf("Digite o valor da variável B:");
+    scanf("%d", &b);
+    
+    a = a + b; 
+    b = a - b; 
+    a = a - b;
+    
+    printf("Dessa forma:\n");
+    printf("A = %d\n", a);
+    printf("B = %d\n", b);
+     
+}
